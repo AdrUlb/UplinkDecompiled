c9ee312a6044251297c8974ae676f5fd4587e27a
Adrian Ulbrich <adrian.ulbrich.2003@gmail.com>
2023-09-16 02:05:30 +0200

    Add missing includes to App.hpp

--- App.hpp
+++ App.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstddef>
+#include <cstdio>
 #include "UplinkObject.hpp"
 #include "UplinkDecompiledTempDefs.hpp"
 
--- UplinkDecompiled.cpp
+++ UplinkDecompiled.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <cassert>
-#include "App.hpp"
 
 __attribute__((constructor))
 static void Init()
 {
-	std::cout << sizeof(App) << std::endl;
-	assert(sizeof(App) == 0x5C4);
+
 }
 
 __attribute__((destructor))
