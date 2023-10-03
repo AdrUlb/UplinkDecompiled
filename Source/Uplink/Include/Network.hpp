335904bd437252393902f06ab7f0b1baeaca08b4
Adrian Ulbrich <adrian.ulbrich.2003@gmail.com>
2023-10-04 01:46:27 +0200

    Declare Network class

--- Source/Uplink/Include/Network.hpp
+++ Source/Uplink/Include/Network.hpp
@@ -1,10 +1,32 @@
 #pragma once
 
 #include <UplinkObject.hpp>
+#include <NetworkScreen.hpp>
+#include <DArray.hpp>
+
+typedef void ClientConnection;
+
+class NetworkServer : UplinkObject
+{
+	int socket;
+	int lastSocket;
+	bool listening;
+	DArray<ClientConnection*> connections;
+};
+
+class NetworkClient : UplinkObject
+{
+	int socket;
+	int clientType;
+	int currentScreen;
+	NetworkScreen* networkScreen;
+};
 
 class Network : UplinkObject
 {
-	char unknown[0x38];
+	NetworkServer server;
+	NetworkClient client;
+	int status;
 
 public:
 	Network();
